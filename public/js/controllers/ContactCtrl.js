7579d643dc32df9967a65083a0021c2b466e1293
Van <vnguyen94@gmail.com>
2015-02-06 14:32:41 -0800

    layout for email

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,3 +1,15 @@
 angular.module('ContactCtrl', []).controller('ContactController', function($scope) {
 
+	$scope.sendEmail = function(user) {
+		if (user.name && user.address && user.subject && user.body) {
+			console.log(user);
+			document.getElementById("contact-button").className += " disabled";
+			document.getElementById("contact-text").className += "glyphicon glyphicon-ok";
+			document.getElementById("contact-text").innerHTML = " Sent!";
+
+		}
+
+		// console.log(user.address);
+	}
+
 });
