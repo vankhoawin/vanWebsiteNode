5628becc7dd06272eee3609ae76bfd79b7bf31b2
Van Nguyen <vnguyen94@gmail.com>
2015-10-12 10:58:34 -0700

    Remove config

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app     = express();
 
 // configuration ===========================================
-var config = require('./app/config');
 var port = process.env.PORT || 8080; // set our port
 
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
